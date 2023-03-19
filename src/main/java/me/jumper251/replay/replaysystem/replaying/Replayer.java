66317caca33a04a13efb06c09ee7b654c672a5c1
TheMiniLuca <TheMiniLuca@gmail.com>
2023-03-19 16:38:25 +0900

    add Event

--- src/main/java/me/jumper251/replay/replaysystem/replaying/Replayer.java
+++ src/main/java/me/jumper251/replay/replaysystem/replaying/Replayer.java
@@ -13,9 +13,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import io.github.theminiluca.api.utils.Colour;
-import net.md_5.bungee.api.ChatMessageType;
-import net.md_5.bungee.api.chat.TextComponent;
+import me.jumper251.replay.api.ReplaySessionRunningEvent;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Entity;
@@ -62,6 +60,12 @@ public class Replayer {
 
     private ReplayingUtils utils;
     private ReplaySession session;
+
+    public int getRunTime() {
+        return runTime;
+    }
+
+
 
     public Replayer(Replay replay, Player watcher) {
         this.replay = replay;
@@ -114,12 +118,8 @@ public class Replayer {
 
             @Override
             public void run() {
-                if (runTime % 10 == 0) {
-                    int level = currentTicks / 20;
-                    int duration1 = replay.getData().getDuration() / 20;
-                    watcher.spigot().sendMessage(ChatMessageType.ACTION_BAR,
-                            TextComponent.fromLegacyText(String.format("%s          %s%02d:%02d / %02d:%02d         %sx%.2f", isPaused() ? Colour.RED + "정지됨" : (currentTicks < duration ? Colour.GREEN + "플레이중" : Colour.DARK_RED + "종료됨"), Colour.AQUA, level / 60, level % 60, duration1 / 60, duration1 % 60, Colour.YELLOW, getSpeed())));
-                }
+                ReplaySessionRunningEvent event = new ReplaySessionRunningEvent(Replayer.this, watcher, duration);
+                Bukkit.getPluginManager().callEvent(event);
 
                 ++runTime;
                 if (Replayer.this.paused) return;
