4cc41346b36ab9761d332f0cdd9f9cd8be8fbe3d
BuildTools <unconfigured@null.spigotmc.org>
2024-01-05 01:34:29 +0900

    update to 1.20.2

--- src/main/java/me/jumper251/replay/replaysystem/utils/entities/PacketNPC.java
+++ src/main/java/me/jumper251/replay/replaysystem/utils/entities/PacketNPC.java
@@ -28,27 +28,27 @@ public class PacketNPC implements INPC{
 	private int id;
 
 	private UUID uuid;
-	
+
 	private String name;
-	
+
 	private int tabMode;
-	
+
 	private WrappedDataWatcher data;
-	
+
 	private WrappedGameProfile profile;
-	
+
 	private Location location, origin;
-	
+
 	private NPCSpawnPacket spawnPacket;
-	
+
 	private float yaw, pitch;
-	
+
 	private Player[] visible;
-	
+
 	private Player oldVisible;
-	
+
 	private List<WrapperPlayServerEntityEquipment> lastEquipment;
-	
+
 	public PacketNPC(int id, UUID uuid, String name) {
 		this.id = id;
 		this.uuid = uuid;
@@ -62,11 +62,11 @@ public class PacketNPC implements INPC{
 			this.spawnPacket = new NPCSpawnPacket(new WrapperPlayServerNamedEntitySpawn());
 		}
 	}
-	
+
 	public PacketNPC() {
 		this(MathUtils.randInt(50000, 400000), UUID.randomUUID(), StringUtils.getRandomString(6));
 	}
-	
+
 	public void spawn(Location loc, int tabMode, Player... players) {
 		this.tabMode = tabMode;
 		this.visible = players;
@@ -80,9 +80,9 @@ public class PacketNPC implements INPC{
 		this.spawnPacket.setPosition(loc.toVector());
 		this.spawnPacket.setYaw(this.yaw);
 		this.spawnPacket.setPitch(this.pitch);
-	
+
 		if(this.data != null) this.spawnPacket.setMetadata(this.data);
-		
+
 		for(Player player : Arrays.asList(players)) {
 			if(tabMode != 0) {
 				getInfoAddPacket().sendPacket(player);
@@ -93,56 +93,56 @@ public class PacketNPC implements INPC{
 				getInfoRemovePacket().sendPacket(player);
 			}
 		}
-		
+
 	}
-	
+
 	public void respawn(Player... players) {
 		this.visible = players;
 		this.spawnPacket.setMetadata(this.data);
 		this.spawnPacket.setPosition(this.location.toVector());
-		
+
 		for (Player player : Arrays.asList(this.visible)) {
 			this.spawnPacket.sendPacket(player);
-			
+
 			for (WrapperPlayServerEntityEquipment equipment : this.lastEquipment) {
 				equipment.sendPacket(player);
 			}
 		}
 	}
-	
+
 	public void despawn() {
 		WrapperPlayServerEntityDestroy destroyPacket = new WrapperPlayServerEntityDestroy();
-		
+
 		if (VersionUtil.isAbove(VersionEnum.V1_17)) {
 			destroyPacket.getHandle().getIntLists().write(0, Arrays.asList(this.id));
 		} else {
 			destroyPacket.setEntityIds(new int[] { this.id });
-		}		
-		
+		}
+
 		for (Player player : Arrays.asList(this.visible)) {
-			if(player != null){				
+			if(player != null){
 				destroyPacket.sendPacket(player);
 			}
 		}
-		
+
 		Arrays.fill(this.visible, null);
-		
+
 	}
-	
+
 	public void remove() {
 		if(NPCManager.names.contains(this.name)) {
 			NPCManager.names.remove(this.name);
 		}
-		
+
 		WrapperPlayServerEntityDestroy destroyPacket = new WrapperPlayServerEntityDestroy();
-		
+
 		if (VersionUtil.isAbove(VersionEnum.V1_17)) {
 			destroyPacket.getHandle().getIntLists().write(0, Arrays.asList(this.id));
 		} else {
 			destroyPacket.setEntityIds(new int[] { this.id });
-		}	
-		
-		if(this.oldVisible != null){				
+		}
+
+		if(this.oldVisible != null){
 			if(this.tabMode == 2){
 				if (VersionUtil.isAbove(VersionEnum.V1_19)) {
 					ProtocolLibrary.getProtocolManager().sendServerPacket(this.oldVisible, getPlayerInfoRemovePacket());
@@ -150,16 +150,16 @@ public class PacketNPC implements INPC{
 					getInfoRemovePacket().sendPacket(this.oldVisible);
 				}
 			}
-				
+
 			destroyPacket.sendPacket(this.oldVisible);
 
 		}
-		
+
 	}
-	
+
 	public void teleport(Location loc, boolean onGround) {
 		this.location = loc;
-		
+
 		WrapperPlayServerEntityTeleport packet = new WrapperPlayServerEntityTeleport();
 
 		packet.setEntityID(this.id);
@@ -169,14 +169,14 @@ public class PacketNPC implements INPC{
 		packet.setPitch(loc.getPitch());
 		packet.setYaw(loc.getYaw());
 		packet.setOnGround(onGround);
-		
+
 		for(Player player : Arrays.asList(this.visible)) {
 			if(player != null) {
 				packet.sendPacket(player);
 			}
 		}
 	}
-	
+
 	public void move(Location loc, boolean onGround, float yaw, float pitch) {
 		WrapperPlayServerRelEntityMoveLook packet = new WrapperPlayServerRelEntityMoveLook();
 		WrapperPlayServerEntityHeadRotation head = new WrapperPlayServerEntityHeadRotation();
@@ -185,7 +185,7 @@ public class PacketNPC implements INPC{
 
 		head.setEntityID(this.id);
 		head.setHeadYaw(((byte)(yaw * 256 / 360)));
-		
+
 		packet.setDx((short) ((loc.getX() * 32 - this.location.getX() * 32) * 128));
 		packet.setDy((short) ((loc.getY() * 32 - this.location.getY() * 32) * 128));
 		packet.setDz((short) ((loc.getZ() * 32 - this.location.getZ() * 32) * 128));
@@ -201,29 +201,29 @@ public class PacketNPC implements INPC{
 			}
 		}
 	}
-	
+
 	public void look(float yaw, float pitch) {
-		  WrapperPlayServerEntityLook lookPacket = new WrapperPlayServerEntityLook();
-		  WrapperPlayServerEntityHeadRotation head = new WrapperPlayServerEntityHeadRotation();
-		  
-		  head.setEntityID(this.id);
-		  head.setHeadYaw(((byte)(yaw * 256 / 360)));
-		  lookPacket.setEntityID(this.id);
-		  lookPacket.setOnGround(true);
-		  lookPacket.setPitch(pitch);
-		  lookPacket.setYaw(yaw);
-		  
-		  for(Player player : Arrays.asList(this.visible)) {
-			  if(player != null) {
-				  lookPacket.sendPacket(player);
-				  head.sendPacket(player);
-			  }
-		  }
+		WrapperPlayServerEntityLook lookPacket = new WrapperPlayServerEntityLook();
+		WrapperPlayServerEntityHeadRotation head = new WrapperPlayServerEntityHeadRotation();
+
+		head.setEntityID(this.id);
+		head.setHeadYaw(((byte)(yaw * 256 / 360)));
+		lookPacket.setEntityID(this.id);
+		lookPacket.setOnGround(true);
+		lookPacket.setPitch(pitch);
+		lookPacket.setYaw(yaw);
+
+		for(Player player : Arrays.asList(this.visible)) {
+			if(player != null) {
+				lookPacket.sendPacket(player);
+				head.sendPacket(player);
+			}
+		}
 	}
-	
+
 	public void updateMetadata() {
 		WrapperPlayServerEntityMetadata packet = new WrapperPlayServerEntityMetadata();
-		
+
 		packet.setEntityID(this.id);
 		if (VersionUtil.isAbove(VersionEnum.V1_19)) {
 			// https://www.spigotmc.org/threads/unable-to-modify-entity-metadata-packet-using-protocollib-1-19-3.582442/
@@ -236,65 +236,65 @@ public class PacketNPC implements INPC{
 		} else {
 			packet.setMetadata(this.data.getWatchableObjects());
 		}
-		
+
 		for (Player player : Arrays.asList(this.visible)) {
 			if (player != null) {
 				packet.sendPacket(player);
 			}
 		}
 	}
-	
+
 	public void animate(int id) {
 		WrapperPlayServerAnimation packet = new WrapperPlayServerAnimation();
-		
+
 		packet.setEntityID(this.id);
 		packet.setAnimation(id);
-		
+
 		for (Player player : Arrays.asList(this.visible)) {
 			if (player != null) {
 				packet.sendPacket(player);
 			}
 		}
 	}
-	
+
 	public void sleep(Location loc) {
 		WrapperPlayServerBed packet = new WrapperPlayServerBed();
-		
+
 		packet.setEntityID(this.id);
 		packet.setLocation(new BlockPosition(loc.toVector()));
-		
+
 		for (Player player : Arrays.asList(this.visible)) {
 			if (player != null) {
 				packet.sendPacket(player);
 			}
 		}
 	}
-	
-	
+
+
 	public void addToTeam(String team) {
 		WrapperPlayServerScoreboardTeam packet = new WrapperPlayServerScoreboardTeam();
-		
+
 		packet.setName(team);
 		packet.setMode(Mode.PLAYERS_ADDED);
 		packet.setPlayers(Arrays.asList(new String[] { this.name }));
-		
+
 		for(Player player : Arrays.asList(this.visible)) {
 			if(player != null) {
 				packet.sendPacket(player);
 			}
 		}
 	}
-	
+
 	private WrapperPlayServerPlayerInfo getInfoAddPacket() {
 		WrapperPlayServerPlayerInfo infoPacket = new WrapperPlayServerPlayerInfo();
 		infoPacket.setAction(EnumWrappers.PlayerInfoAction.ADD_PLAYER);
-		
+
 		WrappedGameProfile profile = this.profile != null ? this.profile : new WrappedGameProfile(this.uuid, this.name);
 		PlayerInfoData data = new PlayerInfoData(profile, 1, EnumWrappers.NativeGameMode.CREATIVE, WrappedChatComponent.fromText(this.name));
 		List<PlayerInfoData> dataList = new ArrayList<PlayerInfoData>();
 		dataList.add(data);
-		
-		infoPacket.setData(dataList);	
+
+		infoPacket.setData(dataList);
 		return infoPacket;
 	}
 
@@ -318,78 +318,78 @@ public class PacketNPC implements INPC{
 
 		return packet;
 	}
-	
+
 	public int getId() {
 		return id;
 	}
-	
+
 	public WrapperPlayServerNamedEntitySpawn getSpawnPacket() {
 		return null;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public UUID getUuid() {
 		return uuid;
 	}
-	
+
 	public void setId(int id) {
 		this.id = id;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public void setUuid(UUID uuid) {
 		this.uuid = uuid;
 	}
-	
+
 	public void setData(WrappedDataWatcher data) {
 		this.data = data;
 	}
-	
+
 	public WrappedDataWatcher getData() {
 		return data;
 	}
-	
+
 	public void setProfile(WrappedGameProfile profile) {
 		this.profile = profile;
 	}
-	
+
 	public void setPitch(float pitch) {
 		this.pitch = pitch;
 	}
-	
+
 	public void setYaw(float yaw) {
 		this.yaw = yaw;
 	}
-	
+
 	public Location getLocation() {
 		return location;
 	}
-	
+
 	public void setOrigin(Location origin) {
 		this.origin = origin;
 	}
-	
+
 
 	public void setLocation(Location location) {
 		this.location = location;
 	}
-	
+
 	public Location getOrigin() {
 		return origin;
 	}
-	
+
 	public Player[] getVisible() {
 		return this.visible;
 	}
-	
+
 	public void setLastEquipment(List<WrapperPlayServerEntityEquipment> list) {
 		this.lastEquipment = list;
-		
+
 	}
 }
